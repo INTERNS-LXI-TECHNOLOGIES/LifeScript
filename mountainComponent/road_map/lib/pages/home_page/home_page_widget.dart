309753834d320cddef6cc83f46c03783ee161023
fahadlxisoft <mhdfhdt@gmail.com>
2024-12-19 23:42:54 +0530

    mountainRoadMap updated!!

--- mountainComponent/road_map/lib/pages/home_page/home_page_widget.dart
+++ mountainComponent/road_map/lib/pages/home_page/home_page_widget.dart
@@ -1,5 +1,6 @@
 import 'package:lottie/lottie.dart';
 
+import 'package:video_player/video_player.dart';
 
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
@@ -52,24 +53,18 @@ class _HomePageWidgetState extends State<HomePageWidget> {
         _isUnlocked = true;
       } else if (_currentAlignment == Alignment(-0.06, 0.36)) {
         _currentAlignment = Alignment(-0.05, 0.24);
-
       } else if (_currentAlignment == Alignment(-0.05, 0.24)) {
         _currentAlignment = Alignment(0.12, 0.04);
         _isSecondLockUnlocked = true;
-
       } else if (_currentAlignment == Alignment(0.12, 0.04)) {
         _currentAlignment = Alignment(0.22, -0.02);
-
       } else if (_currentAlignment == Alignment(0.22, -0.02)) {
         _currentAlignment = Alignment(0.10, -0.14);
-
       } else if (_currentAlignment == Alignment(0.10, -0.14)) {
         _currentAlignment = Alignment(-0.06, -0.35);
         _isThirdLockUnlocked = true;
-
       } else if (_currentAlignment == Alignment(-0.06, -0.35)) {
         _currentAlignment = Alignment(-0.21, -0.38);
-
       } else if (_currentAlignment == Alignment(-0.21, -0.38)) {
         _currentAlignment = Alignment(-0.22, -0.47);
         _isFourthLockUnlocked = true;
@@ -78,8 +73,27 @@ class _HomePageWidgetState extends State<HomePageWidget> {
       } else if (_currentAlignment == Alignment(-0.12, -0.55)) {
         _currentAlignment = Alignment(0.03, -0.78);
         _isFifthhLockUnlocked = true;
+      } else if (_currentAlignment == Alignment(0.03, -0.78)) {
+        _currentAlignment = Alignment(0.14, -0.78);
+        _showCongratsVideo(context);
       }
     });
+  }
+
+  void _showCongratsVideo(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          elevation: 5,
+          backgroundColor: Colors.black,
+          child: VideoPlayerDialog(),
+        );
+      },
+    );
   }
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
@@ -91,29 +105,73 @@ class _HomePageWidgetState extends State<HomePageWidget> {
   }
 
   Widget _showMotivationalPopup(BuildContext context) {
-  // Show a dialog with a motivational message
-  Future.delayed(Duration.zero, () {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('You Got This!'),
-          content: Text('Keep pushing forward, you are doing great!'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text('Okay'),
+    Future.delayed(Duration.zero, () {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0), // Rounded corners
             ),
-          ],
-        );
-      },
-    );
-  });
-  return Container(); // Return an empty container (the popup is handled by the dialog)
-}
-
+            elevation: 5,
+            backgroundColor: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // Wrap content
+                children: [
+                  Icon(
+                    Icons.star_rounded,
+                    color: Colors.orangeAccent,
+                    size: 50.0,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'You Got This!',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Keep pushing forward, you are doing great!',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Close the dialog
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orangeAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(10.0), // Button shape
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                    ),
+                    child: Text(
+                      'Okay',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    });
+    return Container(); // Return an empty container
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -169,56 +227,54 @@ class _HomePageWidgetState extends State<HomePageWidget> {
               ),
             ),
 
-
-
-      Align(
-  alignment: _currentAlignment, // Align based on current alignment
-  child: _currentAlignment == Alignment(-0.2, 0.54)
-      ? Align(
-          alignment: Alignment(-0.2, 0.34), // Position the animation at this point
-          child: Lottie.asset(
-            'assets/jsons/cloudyrain.json', // Cloudy rain animation
-            width: 85,
-            height: 70,
-            repeat: true, // Make sure it repeats
-          ),
-        )
-      : Container(), // Show nothing if alignment doesn't match
-),
-
-Align(
-  alignment: _currentAlignment, // Align based on current alignment
-  child: _currentAlignment == Alignment(-0.05, 0.24)
-      ? Stack(
-          children: [
             Align(
-              alignment: Alignment(0.08, 0.14), // Position the animation at this point
-              child: Lottie.asset(
-                'assets/jsons/thunder.json', // Thunder animation
-                width: 65,
-                height: 60,
-                repeat: true, // Make sure it repeats
-              ),
+              alignment: _currentAlignment, // Align based on current alignment
+              child: _currentAlignment == Alignment(-0.2, 0.54)
+                  ? Align(
+                      alignment: Alignment(
+                          -0.2, 0.34), // Position the animation at this point
+                      child: Lottie.asset(
+                        'assets/jsons/cloudyrain.json', // Cloudy rain animation
+                        width: 85,
+                        height: 70,
+                        repeat: true, // Make sure it repeats
+                      ),
+                    )
+                  : Container(), // Show nothing if alignment doesn't match
             ),
-            // Check if the alignment matches and show popup
-            if (_currentAlignment == Alignment(-0.05, 0.24))
-              _showMotivationalPopup(context), // Show the popup
-          ],
-        )
-      : Container(), // Show nothing if alignment doesn't match
-)
-,
 
+            Align(
+              alignment: _currentAlignment, // Align based on current alignment
+              child: _currentAlignment == Alignment(-0.05, 0.24)
+                  ? Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment(0.08,
+                              0.14), // Position the animation at this point
+                          child: Lottie.asset(
+                            'assets/jsons/thunder.json', // Thunder animation
+                            width: 65,
+                            height: 60,
+                            repeat: true, // Make sure it repeats
+                          ),
+                        ),
+                        // Check if the alignment matches and show popup
+                        if (_currentAlignment == Alignment(-0.05, 0.24))
+                          _showMotivationalPopup(context), // Show the popup
+                      ],
+                    )
+                  : Container(), // Show nothing if alignment doesn't match
+            ),
 
             Align(
               alignment: AlignmentDirectional(-0.1, 0.44),
               child: !_isUnlocked
                   ? Lottie.asset(
-          'assets/jsons/mountain.json', // Mountain animation
-          width: 35,
-          height: 35,
-          repeat: true,
-        )
+                      'assets/jsons/mountain.json', // Mountain animation
+                      width: 35,
+                      height: 35,
+                      repeat: true,
+                    )
                   : Lottie.asset(
                       'assets/jsons/flag.json', // Lock animation
                       width: 50,
@@ -231,11 +287,11 @@ Align(
               alignment: AlignmentDirectional(0.08, 0.14),
               child: !_isSecondLockUnlocked
                   ? Lottie.asset(
-          'assets/jsons/mountain.json', // Mountain animation
-          width: 35,
-          height: 35,
-          repeat: true,
-        )
+                      'assets/jsons/mountain.json', // Mountain animation
+                      width: 35,
+                      height: 35,
+                      repeat: true,
+                    )
                   : Lottie.asset(
                       'assets/jsons/flag.json', // Flag animation
                       width: 50,
@@ -248,11 +304,11 @@ Align(
               alignment: AlignmentDirectional(0.00, -0.21),
               child: !_isThirdLockUnlocked
                   ? Lottie.asset(
-          'assets/jsons/mountain.json', // Mountain animation
-          width: 35,
-          height: 35,
-          repeat: true,
-        )
+                      'assets/jsons/mountain.json', // Mountain animation
+                      width: 35,
+                      height: 35,
+                      repeat: true,
+                    )
                   : Lottie.asset(
                       'assets/jsons/flag.json', // Flag animation
                       width: 50,
@@ -265,11 +321,11 @@ Align(
               alignment: AlignmentDirectional(-0.22, -0.43),
               child: !_isFourthLockUnlocked
                   ? Lottie.asset(
-          'assets/jsons/mountain.json', // Mountain animation
-          width: 35,
-          height: 35,
-          repeat: true,
-        )
+                      'assets/jsons/mountain.json', // Mountain animation
+                      width: 35,
+                      height: 35,
+                      repeat: true,
+                    )
                   : Lottie.asset(
                       'assets/jsons/flag.json', // Flag animation
                       width: 50,
@@ -281,12 +337,12 @@ Align(
             Align(
               alignment: AlignmentDirectional(0.01, -0.64),
               child: !_isFifthhLockUnlocked
-                 ? Lottie.asset(
-          'assets/jsons/mountain.json', // Mountain animation
-          width: 35,
-          height: 35,
-          repeat: true,
-        )
+                  ? Lottie.asset(
+                      'assets/jsons/mountain.json', // Mountain animation
+                      width: 35,
+                      height: 35,
+                      repeat: true,
+                    )
                   : Lottie.asset(
                       'assets/jsons/flag.json', // Flag animation
                       width: 50,
@@ -316,32 +372,30 @@ Align(
                 child: Text("Level Up"),
               ),
             ),
-           
-           
-        
 
-          Align(
-  alignment: AlignmentDirectional(0.36, -0.8),
-  child: CircularPercentIndicator(
-    percent: 1.0,
-    radius: 45.0,
-    lineWidth: 32.0,
-    animation: true,
-    animateFromLastPercent: true,
-    animationDuration: 8000, // Set duration of animation in milliseconds (e.g., 5000 ms = 5 seconds)
-    progressColor: FlutterFlowTheme.of(context).primary,
-    backgroundColor: FlutterFlowTheme.of(context).accent4,
-    center: Text(
-      '100%',
-      style: FlutterFlowTheme.of(context).headlineSmall.override(
-            fontFamily: 'Inter Tight',
-            color: FlutterFlowTheme.of(context).primaryText,
-            fontSize: 28.0,
-            letterSpacing: 0.0,
-          ),
-    ),
-  ),
-),
+            Align(
+              alignment: AlignmentDirectional(0.36, -0.8),
+              child: CircularPercentIndicator(
+                percent: 1.0,
+                radius: 45.0,
+                lineWidth: 32.0,
+                animation: true,
+                animateFromLastPercent: true,
+                animationDuration:
+                    8000, // Set duration of animation in milliseconds (e.g., 5000 ms = 5 seconds)
+                progressColor: FlutterFlowTheme.of(context).primary,
+                backgroundColor: FlutterFlowTheme.of(context).accent4,
+                center: Text(
+                  '100%',
+                  style: FlutterFlowTheme.of(context).headlineSmall.override(
+                        fontFamily: 'Inter Tight',
+                        color: FlutterFlowTheme.of(context).primaryText,
+                        fontSize: 28.0,
+                        letterSpacing: 0.0,
+                      ),
+                ),
+              ),
+            ),
 
             Align(
               alignment: AlignmentDirectional(0.29, 0.39),
@@ -351,7 +405,7 @@ Align(
                 lineWidth: 32.0,
                 animation: true,
                 animateFromLastPercent: true,
-                  animationDuration: 4000, 
+                animationDuration: 4000,
                 progressColor: FlutterFlowTheme.of(context).primary,
                 backgroundColor: FlutterFlowTheme.of(context).accent4,
                 center: Text(
@@ -373,7 +427,7 @@ Align(
                 lineWidth: 32.0,
                 animation: true,
                 animateFromLastPercent: true,
-                  animationDuration: 6000, 
+                animationDuration: 6000,
                 progressColor: FlutterFlowTheme.of(context).primary,
                 backgroundColor: FlutterFlowTheme.of(context).accent4,
                 center: Text(
@@ -395,7 +449,7 @@ Align(
                 lineWidth: 32.0,
                 animation: true,
                 animateFromLastPercent: true,
-                  animationDuration: 7000, 
+                animationDuration: 7000,
                 progressColor: FlutterFlowTheme.of(context).primary,
                 backgroundColor: FlutterFlowTheme.of(context).accent4,
                 center: Text(
@@ -417,7 +471,7 @@ Align(
                 lineWidth: 32.0,
                 animation: true,
                 animateFromLastPercent: true,
-                  animationDuration: 5000, 
+                animationDuration: 5000,
                 progressColor: FlutterFlowTheme.of(context).primary,
                 backgroundColor: FlutterFlowTheme.of(context).accent4,
                 center: Text(
@@ -435,5 +489,47 @@ Align(
         ),
       ),
     );
+  }
+}
+
+class VideoPlayerDialog extends StatefulWidget {
+  @override
+  _VideoPlayerDialogState createState() => _VideoPlayerDialogState();
+}
+
+class _VideoPlayerDialogState extends State<VideoPlayerDialog> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/videos/Congrats.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.setLooping(
+            true); // Ensure the widget is updated once the video is initialized
+        _controller.play(); // Auto-play the video
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose(); // Clean up the controller when done
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? Container(
+            alignment: Alignment.centerLeft, // Align video to the left
+            width: 600, // Set width for resizing the video
+            height: 650, // Set height for resizing the video
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            ),
+          )
+        : Center(child: CircularProgressIndicator());
   }
 }
