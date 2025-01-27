4fac33a7782b8726d109576acde46d1cfd8d8aa6
Sreeja404 <157472694+Sreeja404@users.noreply.github.com>
2025-01-27 16:46:45 +0530

    implemented flutter bloc

---
+++ lifescript001/002-implementations/client/life_script/life_script/habittracker/lib/event/habittrack_success_event.dart
@@ -0,0 +1,9 @@
+import 'package:habittracker/event/habit_track_event.dart';
+
+class HabittrackSuccessEvent extends HabitTrackEvent {
+
+
+
+
+
+}
---
+++ lifescript001/002-implementations/client/life_script/life_script/habittracker/lib/state/habit_success_state.dart
@@ -0,0 +1,8 @@
+import 'package:habittracker/state/habit_track_state.dart';
+
+class HabitSuccessState extends HabitTrackState{
+
+
+
+    
+}
