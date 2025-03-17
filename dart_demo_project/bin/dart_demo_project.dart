ba15b7065f76a0dfab521436208858156ba19c56
Sreeja404 <157472694+Sreeja404@users.noreply.github.com>
2025-03-17 16:13:47 +0530

    working on list Api , completed 10 Api

---
+++ dart_demo_project/bin/dart_demo_project.dart
@@ -0,0 +1,48 @@
+
+// ignore_for_file: unused_element
+
+void main(List<String> arguments) {
+
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  void addNumber(int num) => numbers.add(num);
+  addNumber(6);
+  print(numbers);
+
+  void addMoreNumbers(List<int>  num) => numbers.addAll(num);
+  addMoreNumbers([7 , 8 , 9]);
+  print(numbers);
+
+  insertValue(int index , int value) => numbers.insert(index, value);
+  insertValue(0, 0);
+  print(numbers);
+
+  insertMultipleValues(int index , List<int> values) => numbers.insertAll(index, values);
+  insertMultipleValues(10, [10 , 11 , 12]);
+  print(numbers);
+
+  removeNumber(int num) => numbers.remove(num);
+  removeNumber(12);
+  print(numbers);
+
+  removeNumberAtIndex(int index) => numbers.removeAt(index);
+  removeNumberAtIndex(11);
+  print(numbers);
+
+  removeLastNum() => numbers.removeLast();
+  removeLastNum();
+  print(numbers);
+
+  removeOdd() => numbers.removeWhere((e) => e % 2 != 0);
+  removeOdd();
+  print(numbers);
+
+  retainsEven() => numbers.retainWhere((e) => e % 2 == 0);
+  retainsEven();
+  print(numbers);
+
+  removeValues() => numbers.clear();
+  removeValues();
+  print(numbers);
+
+}
---
+++ dart_demo_project/test/dart_demo_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_demo_project/dart_demo_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
